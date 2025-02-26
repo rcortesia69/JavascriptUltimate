1df2d25a9ab9842ba3cce2190220f9b28ee586ae
Ricardo Cortesia <r.cortesia@gmail.com>
2025-02-26 11:23:19 +0100

    agregar ejercicio de ternario

---
+++ 02-operadores/08-ternario.js
@@ -0,0 +1,10 @@
+// expresion ? 'si es true' : 'si es false';
+
+let edad = 16;
+let acceso = edad > 17 ? 'Acceso permitido' : 'Acceso denegado';
+console.log(acceso); //Acceso denegado
+
+/**
+ * En ester caso este operador funciona muy parecido as un if else, si la condicion es verdadera
+ * Ya que podemos asignar un valor a una variable dependiendo de la condicion
+ */
