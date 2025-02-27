1280511d45ae119b1d6902e3ccfc4f455a908752
Ricardo Cortesia <r.cortesia@gmail.com>
2025-02-27 10:11:43 +0100

    Agregar archivo 06-for.js

---
+++ 03-control-flujo/06-for.js
@@ -0,0 +1,26 @@
+let i = 0;
+while (i < 10) {
+    if ( i % 2== 0){
+        console.log('numero par', i)
+    }
+    i++;
+}
+
+for (inbcializacion; comparacion;terminariteracion) {
+    //codigo
+}
+for (expresiones;expresiones;expresiones){
+    //codigo
+}
+
+/**\
+ * For es un loiop al igual que while, pero ester se compone de 3 expresiones
+ * la primera es la inicializacion de la variable, lyuego una comparaciones y luego una iteracion
+ * aqui se muestra un ejemplo del mismo codigo con while pero en FOR
+ */
+
+for (let i = 0; i < 10; i++) {
+    if ( i % 2== 0){
+        console.log('numero par', i)
+    }
+}
