5e1042c7fec22953ee87e022dd93fb18de745628
Ricardo Cortesia <r.cortesia@gmail.com>
2025-02-28 10:05:43 +0100

    Update 03.js

--- 04-ejercicios/03.js
+++ 04-ejercicios/03.js
@@ -1,5 +1,13 @@
-function getbyIdx(arr, idx){
+/**
+*indice validar que no sea menor a cero y que el elemento exista
+*en el array
+*/
 
+function getbyIdx(arr, idx){
+  if (idx < 0 || arr.length <= idx){
+    return 'elemento no existe'
+  }
+  return arr[idx];
 }
 let resultado = getbyIdx([1, 2], 1);
 console.log(resultado);
