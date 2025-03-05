abe1d71eb7b04aad5de583c44f35d12aa72fe534
Ricardo Cortesia <ricardocortesia@MacBook-Pro-de-Ricardo.local>
2025-03-05 11:42:37 +0100

    Add CHATGPT directory

---
+++ CHATGPT/01.js
@@ -0,0 +1,33 @@
+//**
+// Hare varios ejercicios dadosmpor chatgpt
+//  */
+
+//Variables y tipos de datos:
+//** 1.	Declara tres variables:
+// 	Un número con tu edad.	
+// Un string con tu nombre.	
+// Un booleano que indique si tienes experiencia en programación.
+//  */
+
+let numero = 34;
+let nombre = "Ricardo";
+let tieneExperiencia = true;
+//no se debe usar la palabra boolean en una variable ya que es una palabra reservada
+
+//ejercicio2
+//**
+//  */2.	Cambia el valor de la variable numérica sumándole 5 y muestra el resultado en consola.
+
+let numero = 34;
+numero = numero + 5; // o también: numero += 5;
+console.log(numero);
+
+//tuve una correccion ya que smplemente le habia sumado el 5 a la variable principal en vez de modificarla con un numero nuevo por lo tanto me corrigio
+//asi que se coloca la variable principal y se le suma el 5
+
+//ejercicio3
+
+const IVA = 21;
+let producto = 100;
+let valorProducto = producto*IVA/100;
+console.log(valorProducto);
