4e07d9a60b09ac41e9d16607eb445039d839e005
Ricardo Cortesia <r.cortesia69@gmail.com>
2025-03-11 09:50:57 +0100

    Add .DS_Store and ejercicio05.js

---
+++ CHATGPT/Ejercicios-control-de-flujos/EjerciciosFor/ejercicio05.js
@@ -0,0 +1,6 @@
+let numerousuario = parseFloat(prompt("Introduzca un numero para multiplicar"));
+
+for ( let i = 1; i <= 10; i++){
+    let resultado = i * numerousuario;
+    console.log(` ${i} * ${numerousuario} = ${resultado}`)
+}
